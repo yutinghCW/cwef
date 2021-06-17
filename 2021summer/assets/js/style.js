fbcee9303efadab39736789fd26b1d0d558b0a72
yutingh <yutingh@cw.com.tw>
2021-06-17 14:40:53 +0800

    countdown

--- 2021summer/assets/js/style.js
+++ 2021summer/assets/js/style.js
@@ -188,3 +188,24 @@ $(function () {
 		}
 	});
 });
+
+function makeTimer() {
+	// var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
+		var endTime = new Date("15 July 2021 8:50:00 GMT+08:00");			
+			endTime = (Date.parse(endTime) / 1000);
+		var now = new Date();
+			now = (Date.parse(now) / 1000);
+		var timeLeft = endTime - now;
+		var days = Math.floor(timeLeft / 86400); 
+		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+		if (hours < "10") { hours = "0" + hours; }
+		if (minutes < "10") { minutes = "0" + minutes; }
+		if (seconds < "10") { seconds = "0" + seconds; }
+		$("#days").html("<span class=\"h1 font-weight-500 mr-md-5\">" + days + "</span><span class=\"h6\">日</span>");
+		$("#hours").html("<span class=\"h1 font-weight-500 mr-md-5\">" + hours + "</span><span class=\"h6\">時</span>");
+		$("#minutes").html("<span class=\"h1 font-weight-500 mr-md-5\">" + minutes + "</span><span class=\"h6\">分</span>");
+		$("#seconds").html("<span class=\"h1 font-weight-500 mr-md-5\">" + seconds + "</span><span class=\"h6\">秒</span>");
+}
+setInterval(function() { makeTimer(); }, 1000);
