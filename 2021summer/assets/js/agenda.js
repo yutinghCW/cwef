09d4671c27e13b62398cd3f2c5ed9ae571a4346d
yutingh <yutingh@cw.com.tw>
2021-06-20 22:14:26 +0800

    add speaker

--- 2021summer/assets/js/agenda.js
+++ 2021summer/assets/js/agenda.js
@@ -3,7 +3,12 @@ var agenda = [{
     "intro": "一場疫情，掀起缺料、缺貨、匯率波動，影響之大，前所未見。「貿易衝突」、「病毒」、「供應鏈中斷」，企業都須納入風險管理架構。這意味風險評估概念該被全盤翻新，以順應更難以預估與掌握的世界。面對未來，該如何管理「風險」？",
     "img-1": "3-1.jpg",
     "img-2": "3-2.jpg",
-    "speaker": []
+    "speaker": [{
+        "nameZh": "孫偉真",
+        "nameEn": "Michelle Sun",
+        "titleZh": "</span>美商鄧白氏股份有限公司</span><span>臺灣區總經理</span>",
+        "img": "michelle-sun.jpg"
+    }]
 },{
     "title": "超前突圍：更高速的頻寬公路　更無縫串連的世界",
     "intro": "疫情加深人類對無線網路與頻寬的依賴，5G+AI 如何改變無線（wireless）的未來？在一條更高速，更低延遲的數位公路上，未來有哪些創新與應用的新可能？",
@@ -15,7 +20,22 @@ var agenda = [{
     "intro": "這場防疫戰教會我們，與病毒共存的未來，可能是生活新常態。一座城市，如何提升人民公衛意識，戰勝無所不在病毒？疫情帶給醫療現場的衝撞，撞出那些疫後醫療創新與應用新契機？",
     "img-1": "4-1.jpg",
     "img-2": "4-2.jpg",
-    "speaker": []
+    "speaker": [{
+        "nameZh": "柯文哲",
+        "nameEn": "Wen-je Ko",
+        "titleZh": "</span>台北市政府</span><span>市長</span>",
+        "img": "wenje-ko.jpg"
+    },{
+        "nameZh": "黃士維",
+        "nameEn": "Wayne Shih-Wei Huang",
+        "titleZh": "</span>彰濱秀傳紀念醫院</span><span>院長</span>",
+        "img": "wayne-huang.jpg"
+    },{
+        "nameZh": "林道隆",
+        "nameEn": "Steven D. Lin",
+        "titleZh": "</span>思必瑞特生技股分有限公司</span><span>董事長兼任總經理</span>",
+        "img": "steven-lin.jpg"
+    }]
 },{
     "title": "超前突圍：不只造車　電動車生態系來了！",
     "intro": "電動車已經成為全球新顯學。疫情也阻擋不了台灣在全球電動車競賽中前進，從兩輪到四輪，台灣正打造自己的電動車系統，甚至輸出海外打國際戰。",
@@ -27,7 +47,17 @@ var agenda = [{
     "intro": "在疫情中重創的服務業，用科技突圍，靠更貼近人心的服務抓住消費者的心，更是打破場域的新零售體驗。大量在家的工作時間除了帶動外送服務順勢崛起，頂級零售業也必須思考服務數位轉型，把有溫度的實際體驗帶進客戶面前。",
     "img-1": "5-1.jpg",
     "img-2": "5-2.jpg",
-    "speaker": []
+    "speaker": [{
+        "nameZh": "蘇奧然",
+        "nameEn": "Oliver Su",
+        "titleZh": "</span>德載國際不動產</span><span>創辦人與產品長</span>",
+        "img": "oliver-su.jpg"
+    },{
+        "nameZh": "陳星豪",
+        "nameEn": "Jason Chen",
+        "titleZh": "</span>軒饌廚坊股份有限公司</span><span>首席執行長兼共同創辦人</span>",
+        "img": "jason-chen.jpg"
+    }]
 },{
     "title": "超前突圍：疫後新生活",
     "intro": "「淨零碳排」已是全球永續共識。極端氣候已經到家門口，從研發到生產製造如何環境友善？面對疫後世界的綠色浪潮，企業如何將ESG理念，貼合未來成長？",
--- 2021summer/assets/js/speaker.js
+++ 2021summer/assets/js/speaker.js
@@ -1,239 +1,43 @@
 var data = [{
-    "nameZh": "湯姆．斯丹迪奇",
-    "nameEn": "Tom Standage",
-    "titleZh": "《經濟學人》The World in 2021總編輯",
-    "titleEn": "Deputy Editor, The Economist; Editor, “The World in 2021” ",
-    "img": "1-1-standage.jpg",
-    "intro": "湯姆．斯丹迪奇(Tom Standage)為英國《經濟學人》雜誌副總編輯與數位總編輯，是該雜誌四人經營團隊之一，也身兼《經濟學人》年度重量級刊物《The World In 全球大趨勢》的總編輯。他以數位總編輯的身份，主管經濟學人的數位策略，以及官網與各種數位平台的經營。畢業於英國牛津大學的他，曾任職衛報與每日電訊報，從主跑科學新聞的基層記者做起，加入經濟學人至今已超過20年。"
+    "nameZh": "孫偉真",
+    "nameEn": "Michelle Sun",
+    "titleZh": "美商鄧白氏股份有限公司臺灣區總經理",
+    "titleEn": "General Manager, Dun & Bradstreet",
+    "img": "michelle-sun.jpg",
+    "intro": "孫偉真領導鄧白氏成為企業數位轉型標竿，過去 20 多年，她的團隊協助臺灣企業邁向大數據時代，透過雲端整合全球商業數據實現數據創新，讓鄧白氏成為幫助臺灣企業實現獲利成長的關鍵夥伴。孫偉真為美國加州州立大學富爾頓分校企業管理學士，臺灣大學 EMBA 管理碩士。"
 },{
-    "nameZh": "黃樂仁",
-    "nameEn": "Laura Huang",
-    "titleZh": "哈佛大學商學院副教授",
-    "titleEn": "Associate Professor, Harvard Business School",
-    "img": "1-2-huang.jpg",
-    "intro": "目前為哈佛大學商學院副教授的黃樂仁(Laura Huang)，也是哈佛商學院第一位台裔女性教授。進入哈佛商學院之前，於賓州大學華頓商學院擔任助理教授，2016年，受美國知名商學院網站Poets & Quants評選為「全球頂尖40歲以下40位商學院教授」。曾受邀為Google、Uber等企業演講、進行組織改造，並為多家新創企業擔任顧問。著有《隱性優勢》，剖析企業與個人應如何將對自身不利的情勢，逆轉為獨特的優勢。"
+    "nameZh": "柯文哲",
+    "nameEn": "Wen-je Ko",
+    "titleZh": "台北市政府市長",
+    "titleEn": "Mayor, Taipei City Government",
+    "img": "wenje-ko.jpg",
+    "intro": "現任台北市市長，曾任台大醫院外科部主治醫師兼創傷醫學部主任、台大醫學院教授。柯文哲擁有深厚醫學背景，主張政治就是應該落實在人民生活的每一天，堅持從施政點滴中不斷精進。柯文哲為台灣大學醫學院臨床醫學研究所博士。"
 },{
-    "nameZh": "帕斯卡爾．卡尼",
-    "nameEn": "Pascal Cagni",
-    "titleZh": "法國商務部國際投資大使",
-    "titleEn": "Founder & CEO, C4 Ventures\nFrench Ambassador, International Investment\nChairman of Business France",
-    "img": "1-3-cagni.jpg",
-    "intro": "帕斯卡·卡尼（Pascal Cagni）於2017年9月被法國總統馬克宏任命為法國國際投資大使，並擔任法國商務部董事會主席。同時他也是C4 Ventures風險基金的創辦人。\n他曾被蘋果創辦人賈伯斯親自延攬，從2000年至2012年擔任蘋果歐洲、中東和非洲地區總經理兼副總裁，先前也曾擔任Packard Bell歐洲主管，與台灣淵源頗深。卡尼畢業於巴黎高等政治學院（Sciences Po），並曾就讀巴黎高等商學院（HEC Paris）的MBA課程。"
+    "nameZh": "黃士維",
+    "nameEn": "Wayne Shih-Wei Huang",
+    "titleZh": "彰濱秀傳紀念醫院院長",
+    "titleEn": "Superintendent, Chang Bing Show Chwan Memorial Hospital",
+    "img": "wayne-huang.jpg",
+    "intro": "彰化秀傳醫院二代，曾任秀和基金會董事、明士股份有限公司董事長，黃士維現為彰濱秀傳紀念醫院院長、秀傳亞洲遠距微創手術中心院長，同時也是一位一般外科主治醫師。2019 年底，帶領醫院與醫療科技創投比翼資本共同成立「比翼秀傳聯名基金」，打造臺灣第一檔醫院創投，並親自操盤基金投資。黃士維為美國杜蘭大學公共衛生所碩士、東京女子醫科大學博士畢業。"
 },{
-    "nameZh": "周建宏",
-    "nameEn": "Joseph Chou ",
-    "titleZh": "資誠聯合會計師事務所所長暨聯盟事業執行長",
-    "titleEn": "Chairman & CEO, PricewaterhouseCoopers Taiwan",
-    "img": "1-4-chou.jpg",
-    "intro": "周建宏為東海大學物理系畢業、美國密蘇里大學會計碩士。專長包括台美上市櫃輔導、跨國併購諮詢、公司治理及內控制度諮詢、企業變革管理、企業社會責任報告諮詢等。曾經擔任資誠聯合會計師事務所執行長、審計服務部營運長、PwC New York Global Capital Market Group partner、PwC San Jose A ssurance manager 等。"
+    "nameZh": "林道隆",
+    "nameEn": "Steven D. Lin",
+    "titleZh": "思必瑞特生技股分有限公司董事長兼任總經理",
+    "titleEn": "Chairman/General Manager, Spirit Scientific Co., Ltd",
+    "img": "steven-lin.jpg",
+    "intro": "曾任華盛頓大學附屬醫院擔任急救外科醫師，林道隆具臨床實務經驗，也曾擔任多家國際生技醫療公司的顧問，並曾在國際藥廠輝瑞負責過臨床專案。2007 年受邀參與腎臟科用藥研發，進入台灣寶齡富錦歷經產品開發、臨床、事業發展。之後創立思必瑞特生技，並成功開發 PLT 療程之應用，開始在生醫療領域的布局。林道隆為美國史丹佛大學醫學院畢業。"
 },{
-    "nameZh": "郭水義",
-    "nameEn": "Shui Yi Kuo",
-    "titleZh": "中華電信股份有限公司總經理",
-    "titleEn": "President, Chunghwa Telecom",
-    "img": "2-1-kao.jpg",
-    "intro": "郭水義是中華電信首位財會專長的總經理，自2001年從勤業管理顧問公司進入中華電信，由經營規劃處、投資事業處、會計處一路晉升為投資長及財務長，對轉投資公司經營及績效管理有豐富經驗，主導過神腦、KKBOX、愛爾達、中華精測、是方電訊及勤崴科技等中華電信重要的轉投資案。目前帶領中華電信在5G時代「廣結盟」，創造共贏的5G生態系。"
+    "nameZh": "蘇奧然",
+    "nameEn": "Oliver Su",
+    "titleZh": "德載國際不動產創辦人與產品長",
+    "titleEn": "Founder & CPO, FUNWOO",
+    "img": "oliver-su.jpg",
+    "intro": "曾任人工智慧推薦引擎公司 Zofari 技術長，研發 Pandora for Place 人工智慧推薦引擎，2014 年成功被雅虎收購。並在矽谷領導開發多項軟體專案如雲端架構管理系統、自動駕駛數據雲平台、企業級網路異常偵測大數據平台。蘇奧然將矽谷新創模式帶回台灣，創立 Funwoo，運用雲端大數據與人工智慧，簡化跟 e 化台灣房產交易過程，提升客戶的產品體驗。蘇奧然為美國西北大學資訊科學博士。"
 },{
-    "nameZh": "埃敏",
-    "nameEn": "Tareq Amin",
-    "titleZh": "樂天移動董事、副總裁兼科技長",
-    "titleEn": "Reprsentative Director, Executive Vice President & Chief Technology Officer, Rakuten Mobile, Inc./Group Executive Vice President & Chief Architecture Officer, Rakuten, Inc.",
-    "img": "2-2-amin.jpg",
-    "intro": "約旦裔在美國受教育的埃敏，是樂天移動以後進者之姿挑戰軟銀、NTT Docomo的關鍵人物。曾在美國Ｓprint、華為，及印度電信公司Ｒeliance Jio擔任重要職位的他，曾被美國電信專業網站Fierce Wireless選為「2019無線科技最有權力人物」。2018年，被樂天創辦人三木谷浩史挖角，為剛拿到5G電信執照的樂天移動領軍發展，其最重要策略之一，即是連結台灣資訊電子業的製造實力，攜手搶進5G新時代。"
-},{
-    "nameZh": "王煒",
-    "nameEn": "James Wang",
-    "titleZh": "中磊電子總經理",
-    "titleEn": "CEO & President, Sercomm Corporation",
-    "img": "2-3-wang.jpg",
-    "intro": "王煒在科技產業、網路通訊領域有近30年的經歷，於2000年加入中磊電子並擔任總經理至今，不斷締造營運佳績，帶領中磊年營收從5億元一路成長到近年高峰達386億元，是台灣第一個營收突破300億元的網通廠。擁有美國哈佛大學商業管理碩士、美國卡內基美隆大學碩士雙學位的他，除嫻熟網通專業技術，更以管理見長，曾榮獲2014安永企業家獎年度大獎、2020機構投資人雜誌「最佳執行長」等獎項。"
-},{
-    "nameZh": "汪佳慧",
-    "nameEn": "Grace Wang",
-    "titleZh": "英特爾公司業務行銷暨公關事業群副總裁  ",
-    "titleEn": "Vice President & GM, Taiwan, Sales and Marketing Group, Intel Corporation ",
-    "img": "2-4-wang.jpg",
-    "intro": "汪佳慧在英特爾服務資歷逾20年，曾擔任業務、行銷、與技術支援等管理職位，具備深厚的晶片設計與軟體應用工程技術底蘊，曾獲英特爾成就獎（Intel Achievement Award）殊榮。 過去汪佳慧曾擔台灣ODM產業生態系業務總監和亞太技術行銷業務總監等職位，領導團隊推動PC、物聯網與資料中心發展，致力為全球客戶提供成長動能。\n"
-},{
-    "nameZh": "林佳龍",
-    "nameEn": "Chia Lung Lin ",
-    "titleZh": "交通部長",
-    "titleEn": "Minister, Ministry of Transportation and Communications",
-    "img": "3-1-lin.jpg",
-    "intro": "台大政治學碩士、美國耶魯大學人文哲學雙碩士、政治學博士畢業後，林佳龍即活躍於台灣政治圈。曾任總統府副秘書長、行政院發言人、新聞局局長、台中市長等職，2019年1月出任交通部部長至今。\n"
-},{
-    "nameZh": "阮慕驊",
-    "nameEn": "Danny Ruan",
-    "titleZh": "專業財經節目主持人",
-    "titleEn": "Financial program host",
-    "img": "3-2-ruan.jpg",
-    "intro": "從體育、財經，到證券記者，因為長期沉浸財經和金融情勢的報導與分析，讓阮慕驊成為頗具專業形象的資深財經專家，而其深刻和犀利的評論和見解，也使他成為各談話性節目的常客，進而成為知名電視與廣播節目主持人。目前為News98財經一路發等節目主持人，對投資及產業發展趨勢常有獨到觀察與見解。"
-},{
-    "nameZh": "劉揚偉",
-    "nameEn": "Young Liu",
-    "titleZh": "鴻海科技集團董事長",
-    "titleEn": "Chairman, Hon Hai Technology Group (Foxconn)",
-    "img": "3-3-lin.jpg",
-    "intro": "2007年加入鴻海科技集團，加入鴻海之前劉揚偉是創業家，曾於美國創立主機板公司Young Micro、IC設計公司ITE及ITeX，曾任鴻海董事長特助、鴻海S次集團總經理、日本夏普董事；2019年接任董事長後，肩負全球EMS龍頭從「製造鴻海」轉型「科技鴻海」的重任，並創辦「鴻海研究院」、確立「電動車、數位健康、機器人」三大產業做為鴻海新營運目標。"
-},{
-    "nameZh": "林東閔",
-    "nameEn": "Tony Lin",
-    "titleZh": "宏佳騰智慧電車執行長",
-    "titleEn": "CEO, AEONMOTOR CO., LTD.",
-    "img": "3-3-liu.jpg",
-    "intro": "7年級生的林東閔，早在30歲就擔任宏佳騰營運中心副總經理暨品牌執行長；並成立CROXERA智慧科技新創品牌，發展機車聯網裝置與系統，目標將機車王國升級為智慧安全電車王國。經歷比學歷精采，大一肄業後經歷電腦、電信、網路資通訊科技、教育等產業，2014年畢業於北京清華大學企管研修班。科技、大自然與音樂愛好者，相信科技的力量將改善人類生活，但更重視使用者體驗。 "
-},{
-    "nameZh": "張永昌",
-    "nameEn": "YC Chang",
-    "titleZh": "遠通電收股份有限公司執行董事兼總經理",
-    "titleEn": "Managing Director, Far Eastern Electronic Toll Collection Co., Ltd. ",
-    "img": "3-4-chang.jpg",
-    "intro": "張永昌曾任陽明海運資訊公司資訊長、財務長，在交通運輸、企業管理、電訊科技等資訊應用，皆有實戰經驗。曾在成功大學交通管理學系暨電信管理研究所任教，投入電腦、營運、財務、運輸、資訊、網路與電信等多方知識領域的整合，並帶領遠通電收完成ETC系統。現亦對新創懷有使命，擔任AAMA台北搖籃計畫第九期導師。"
-},{
-    "nameZh": "簡立峰",
-    "nameEn": "Lee Feng Chien",
-    "titleZh": "前Google台灣董事總經理/iKala董事",
-    "titleEn": "Former Managing Director, Google Taiwan/\nBoard Director, iKala ",
-    "img": "4-1-chien.jpg",
-    "intro": "簡立峰博士是台灣軟體科技及新創產業發展的重要推手。曾任Google台灣董事總經理，也是Google台灣的第一位員工，在職期間帶領台灣辦公室成為Google亞太區最大研發基地。2020年自Google退休後，目前擔任iKala、Appier等AI新創公司董事，持續協助推動台灣新創產業發展與科技人才培育。過去簡博士亦曾任中研院資訊科學研究所副所長、台灣大學資管系合聘教授、微軟亞洲研究院技術顧問等職，也是中文搜尋、自然語言處理領域專家。"
-},{
-    "nameZh": "顏哲淵",
-    "nameEn": "Jeryuan Yan ",
-    "titleZh": "NextDrive 聯齊科技創辦人暨執行長",
-    "titleEn": "Founder & CEO, NextDrive",
-    "img": "4-2-yan.jpg",
-    "intro": "顏哲淵在創辦NextDrive 聯齊科技前，也曾創辦 Ninecom 並擔任執行長、並曾於美商 Fresco Logic 擔任台灣區總經理。2013 年顏哲淵創立 NextDrive，開發出世界最小的智慧能源閘道器 Cube，並率領團隊打造能應用在家庭、社區、公有場域的能源管理解決方案。多樣化又貼近用戶的服務也受到日本東京電力、東北電力，以及台電等多家大型電力公司採用；同時推動多項場域能源管理實驗專案，致力實現智慧、綠能、永續的生活。"
-},{
-    "nameZh": "程世嘉",
-    "nameEn": "Sega Cheng",
-    "titleZh": "iKala共同創辦人暨執行長",
-    "titleEn": "Co-Founder & CEO, iKala",
-    "img": "4-3-cheng.jpg",
-    "intro": "程世嘉曾在Google擔任軟體工程師長達6年，最後選擇創業，一路帶領網路K歌平台新創iKala，轉型成以AI技術為核心的行銷公司。iKala現在旗下有 iKala Cloud 及 iKala Commerce 等雲端及新零售服務與產品，服務超過400 家企業及 15,000 家廣告主及品牌主，其中包含多家財星 500 大企業，服務範圍跨越 台、星、泰、越、日、港、菲，及大馬等8 個國家和超過 12 種產業。"
-},{
-    "nameZh": "王易如",
-    "nameEn": "Iru Wang",
-    "titleZh": "行動貝果營運長暨共同創辦人\n",
-    "titleEn": "COO & Co-founder, MoBagel",
-    "img": "4-4-wang.jpg",
-    "intro": "出生於1988年的王易如，台大電機系畢業後即赴美留學，並在史丹佛大學取得電機碩士學位後，留在矽谷創業。後憑藉著自身矽谷創業與募資經驗，參與和協助來自台灣的行動貝果(MoBagel)進入創業加速器500 Startups，並在五年內累積獲投資金額超過新台幣兩億五千萬。行動貝果以自動化學習（AutoML）技術，協助製造、零售、電信、金融業者進行商業預測，預測內容包括供應鏈需求、消費行為、人力資源等。包括軟銀集團、可口可樂都是行動貝果的客戶。"
-},{
-    "nameZh": "陳鳳馨",
-    "nameEn": "Fengshing Chen",
-    "titleZh": "新聞評論員",
-    "titleEn": "Commentator",
-    "img": "5-1-chen.jpg",
-    "intro": "政大新聞系畢業的陳鳳馨，堪稱全方位資深媒體人。因為長期以來對於國際趨勢、商業財經、政治脈動的均有深度研究及評論觀察，不僅是各大政論及財經談話性節目的常客，自身也是知名節目主持人，手上節目包括TVBS《國民大會》、News 98 廣播節目《財經起床號》等。與此同時，在每週五的廣播節目中，已連續17年不間斷為數十萬聽眾，精闢解讀優質好書，並訪問過數百位暢銷作家，是少數具備能寫、能說，橫跨電視、報紙、廣播的媒體人。"
-},{
-    "nameZh": "侯友宜",
-    "nameEn": "Yu Ih Hou",
-    "titleZh": "新北市市長",
-    "titleEn": "Mayor, New Taipei City",
-    "img": "5-2-hou.jpg",
-    "intro": "在2020年《天下雜誌》縣市首長施政滿意度民調超過85%的侯友宜，曾任兩屆新北市副市長、內政部警政署長、中央警察大學校長。出身基層刑警，他用獨門「抓豬哲學」，過去在犯罪現場捕捉犯人，如今則用心傾聽市民需求，以「安、居、樂、業」四大施政主軸，在治理人口逾400萬的全台第一大城市時，不僅締造過往罕見的市民高滿意度，更積極引領新北市邁向永續城市，是全台第一個公布自願性地方永續發展報告(Voluntary Local Review, VLR)的縣市。"
-},{
-    "nameZh": "鄭文燦",
-    "nameEn": "Wen Tsan Cheng",
-    "titleZh": "桃園市市長",
-    "titleEn": "Mayor, Taoyuan City",
-    "img": "5-3-cheng.jpg",
-    "intro": "鄭文燦自2014年擔任桃園市長至今，是桃園市升格直轄市後首任市長。個性圓融、整合性高，加上勤跑基層，不僅地方人士不分黨派都對他予高度肯定，在歷年《天下雜誌》縣市首長施政滿意度調查排名也經常名列前茅。他致力提升市民生活品質，著重民眾有感的施政外，亦展現強大執行力，如停滯多年的航空城、鐵路地下化等「三心六線」工程都將啟動，引領桃園邁入大建設時代，從過去衛星邊陲的角色搖身成為核心大都會。"
-},{
-    "nameZh": "吳明賢",
-    "nameEn": "Ming Shiang Wu",
-    "titleZh": "臺大醫院院長",
-    "titleEn": "Superintendent, National Taiwan University Hospital",
-    "img": "6-1-wu.jpg",
-    "intro": "除了台大醫院院長外，吳明賢也是現任台灣消化系醫學會理事長，在幽門螺旋桿菌和胃腸道癌症研究領域具有世界級地位。他曾率團隊和台灣幽門桿菌及胃腸疾病聯盟，進行一系列多中心隨機臨床試驗，發表幽門螺旋桿菌除菌與胃癌防治經驗，提供全世界胃癌防治準則，研究著作見於 《刺絡針》、《英國醫學期刊》等頂尖醫學期刊。他曾三度獲科技部傑出獎及亞太消化系學會新領袖肯定，今年則獲教育部學術獎殊榮。"
-},{
-    "nameZh": "張鴻仁",
-    "nameEn": "Hong Jen Chang",
-    "titleZh": "台灣研發型生技新藥發展協會理事長  ",
-    "titleEn": "Chairman , Taiwan Research-based Biopharmaceutical Manufacturers Association",
-    "img": "6-2-chang.jpg",
-    "intro": "自醫學系畢業後，張鴻仁即長期投入台灣的公衛行政體系，在擔任健保局總經理時，經歷台灣最艱困的公衛危機：SARS。他引進第一台體溫監測系統，任內完成健保IC卡建置計畫。曾任衛生局副署長、衛生署疾病管制局首任局長與健保局總經理等職位。現為台灣研發型生技新藥發展協會理事長，也是上騰生技股份有限公司、上準微流體公司與雅祥生技醫藥公司的董事長。"
-},{
-    "nameZh": "張智威",
-    "nameEn": "David C. Chang",
-    "titleZh": "哈佛大學附設麻省總醫院(MGH)外科副教授",
-    "titleEn": "Associate Professor of Surgery, Massachusetts General Hospital,Harvard Medical School",
-    "img": "6-3-chang.jpg",
-    "intro": "小學六年級即從台灣移居美國的張智威(David Chang)，目前是在美國麻省總醫院擁有甚高地位的外科副教授。儘管身處醫界，但他尤重跨界思維，致力挖掘醫學研究背後的隱含偏差，提倡台灣成為全球華人的醫學研究重鎮。因為兼具神經生物學、公衛訓練，他也是美國國防部國會指定醫學研究計劃顧問、美國國家衛生研究院緊急創傷研究圓桌計劃顧問。張智威所在的美國麻省總醫院，是哈佛醫學院旗下最早、規模最大的附屬醫院，至今已出13位諾貝爾獎得主，是全球頂尖的癌症治療、神經外科等醫療與研究重鎮。"
-},{
-    "nameZh": "曹世綸",
-    "nameEn": "Terry Tsao",
-    "titleZh": "SEMI全球行銷長暨台灣區總裁",
-    "titleEn": "Global Chief Marketing Officer, SEMI\nPresident, SEMI Taiwan\n",
-    "img": "7-1-tsao.jpg",
-    "intro": "曹世綸領導SEMI全球行銷與市場經營計畫，同時負責SEMI在台灣區的所有活動計劃、產品和服務內容。他同時協助各項國際計劃，提供全球會員全方位服務。在此之前為SEMI東南亞區總裁，負責SEMI東南亞的營運拓展與經營規劃長達六年。加入SEMI前，曾擔任IDC國際數據資訊台灣區董事總經理，以及趨勢科技亞太區行銷協理。"
-},{
-    "nameZh": "李謀偉",
-    "nameEn": "Bowei Lee",
-    "titleZh": "李長榮集團總裁",
-    "titleEn": "Chairman,LCY GROUP",
-    "img": "7-2-lee.jpg",
-    "intro": "李長榮集團總裁李謀偉帶領國際石化工業企業，秉持創新、差異化與國際化經營策略，開拓再生能源、環境友善材料版圖；透過積極參與具影響力的產業與教育組織，成立李長榮教育基金會推動台灣創新能量，並擔任台灣化學產業協會理事長與其他重要產業組織顧問，是領導產業向上發展的重要推手。"
-},{
-    "nameZh": "黃育徵",
-    "nameEn": "Charles Huang",
-    "titleZh": "循環台灣基金會創辦人暨董事長",
-    "titleEn": "Founder & Chairman, Circular Taiwan Network",
-    "img": "7-3-huang.jpg",
-    "intro": "2015年創辦循環台灣基金會，是台灣第一個引進並推廣循環經濟的非營利組織。五年來積極在公部門、企業與媒體間，推廣以循環經濟觀念取代傳統經濟發展與企業成長思惟，替資源有限的台灣，開闢一條永續發展的新路，被政府採納為經濟發展策略之一。擔任台糖董事長期間，將台糖打造為台灣養豬事業轉型循環經濟的領頭羊，堪稱台灣的循環經濟之父。過去曾任台橡公司董事長暨執行長、欣陸投資控股公司、新鼎系統等多家上市公司董事。  "
-},{
-    "nameZh": "林業軒",
-    "nameEn": "Yeh Hsuan Lin",
-    "titleZh": "驚喜製造股份有限公司董事長暨商業發展總監",
-    "titleEn": "Chairman & Head of Business Development, \nSurprise Lab",
-    "img": "8-lin.jpg",
-    "intro": "身兼旅行者、美食愛好者與行銷人等多重身份，喜歡找到不同領域的交集，並跨界合作。讀過台大農經系、倫敦皇后瑪麗學院行銷所，返台後與夥伴陳心龍共同創辦了「驚喜製造」，推出「無光晚餐、一人餐桌table for ONE、微醺大飯店」等結合表演、燈光音效與餐飲的沈浸式體驗活動，致力讓城市往更有趣的方向滾動。"
-},{
-    "nameZh": "陳佳文",
-    "nameEn": "James Chen",
-    "titleZh": "中國信託商業銀行總經理",
-    "titleEn": "President & CEO，CTBC Bank Co., Ltd.",
-    "img": "9-1-chen.jpg",
-    "intro": "經歷豐富的金融專業經理人，陳佳文總經理帶領國際化布局完整的中國信託，持續創新服務。曾任中國信託金控總經理、中國信託資產管理董事長、荷蘭銀行台北分行副總經理等職，2015 年則獲得國際財經專業機構《Asiamoney》（亞洲貨幣）評選「臺灣最佳經理人」。"
-},{
-    "nameZh": "陳政鴻",
-    "nameEn": "Jay Chen",
-    "titleZh": "恆隆行貿易股份有限公司董事長",
-    "titleEn": "Chairman, HLH CO., LTD",
-    "img": "9-2-chen.jpg",
-    "intro": "陳政鴻為企業二代接班人，歷時24年帶領恆隆行貿易公司逐步轉型，拓展代理品牌範疇。從專營相機周邊商品的代理商，到成為精品家電的代名詞，代理範圍橫跨環境淨化、廚房家電、廚房用品、健康美容與生活日用等五大類別。因為體認代理品牌與消費者之間溝通的橋樑角色，他很看重透過優化消費者接觸點旅程，提供有感服務，並期許甫屆滿60周年的恆隆行，未來能持續創造風潮，引領使用者體驗更進化的美好生活。"
-},{
-    "nameZh": "陳明明",
-    "nameEn": "Ming Chen",
-    "titleZh": "KKday執行長",
-    "titleEn": "CEO,KKday",
-    "img": "9-3-chen.jpg",
-    "intro": "陳明明是一名熱愛自由行的旅人，更是懷抱遠大夢想、敢於接受挑戰並開創新局的成功連續創業家。他曾於2000年共同創辦易遊網，2003年共同創辦燦星旅遊網（ezTravel），2010年購併易飛網（ezfly）。\n2014年他創立 KKday，鎖定自由行旅人市場，提供世界各地的多樣化在地體驗行程，業務覆蓋全球逾90個國家、550個城市。2020年9月，KKday宣布完成7,500萬美元（約為新台幣22.5億元）C輪融資。"
-},{
-    "nameZh": "周俊吉",
-    "nameEn": "Chun Chi Chou",
-    "titleZh": "信義企業集團創辦人",
-    "titleEn": "Founder, Sinyi Realty ",
-    "img": "10-1-chou.jpg",
-    "intro": "帶領信義房屋走過40個年頭的周俊吉，不僅是台灣房仲業的龍頭，更因誠信立業的主張、先義後利的堅持，為行業立下新標準。而其對企業倫理的大力倡議與資助，並率地方創生風氣之先，帶領信義房屋對國內社區營造與發展有長期且持續的投入，都讓他成為實踐企業社會責任(CSR)的標誌性企業家。"
-},{
-    "nameZh": "周志宏",
-    "nameEn": "Jesse Chou",
-    "titleZh": "台達電子永續長暨發言人",
-    "titleEn": "Chief Sustainability Officer & Spokesperson, Delta Electronics, Inc.",
-    "img": "10-2-chou.jpg",
-    "intro": "2004年加入台達電子，周志宏目前擔任台達永續長暨發言人，以台達的電力電子核心技術為基礎，對外溝通台達的創新研發佈局及成果，將企業的創新與永續經營緊密連結，使台達穩健維繫綠能科技專業的形象。自2011年起，台達即連續獲選道瓊永續指數 (DJSI World Index)成分股至今；在2020年發布的CDP (原碳揭露專案) 年度評比鍾，更榮獲台灣業界首度氣候變遷與水安全雙「A」領導級企業，堪稱國內永續領域的典範企業。"
-},{
-    "nameZh": "黃一展",
-    "nameEn": "Jack Huang",
-    "titleZh": "菁鏈科技(本住)執行長",
-    "titleEn": "CEO, Eliconn Co., Ltd. (SHOULD)",
-    "img": "10-3-huang.jpg",
-    "intro": "黃一展為現任菁鏈科技(本住)執行長，曾先後在聯合國組織團體工作，包含亞太投資貿易處、資訊科技辦公室，及維和部隊於剛果 、索馬利亞的特派團，關注聯合國永續發展目標(SDGs)，深耕普惠金融、環境與氣候、區塊鏈技術與社會新創。2020年共同創辦本住，與團隊打造新型能源管理系統，整合科技與平台，讓節能永續理念融入建築空間、供應鏈與日常生活。"
-},{
-    "nameZh": "鄭涵睿",
-    "nameEn": "Harris Cheng",
-    "titleZh": "綠藤生機共同創辦人暨執行長",
-    "titleEn": "Co-founder & CEO, Greenvines Biotech Co., Ltd",
-    "img": "10-4-cheng.jpg",
-    "intro": "鄭涵睿是台灣在永續領域別具代表性的新一代創業家。本著追求正向價值的理想，他在30歲前夕離開外商銀行，與兩位大學同學創辦綠藤生機，從最初賣活有機芽菜，而後逐漸跨足到洗沐浴與保養品領域，強調從研發到包裝均以環境友善為最高指導原則。綠藤生機是台灣第三家獲國際認證的B型企業，產品強調「永續、純淨、科學」，力求在推廣產品的同時，又能倡導永續及公平貿易的理念。"
+    "nameZh": "陳星豪",
+    "nameEn": "Jason Chen",
+    "titleZh": "軒饌廚坊股份有限公司首席執行長兼共同創辦人",
+    "titleEn": "CEO & Co- Founder, Just Kitchen Holdings Crop.",
+    "img": "jason-chen.jpg",
+    "intro": "在國際資本市場和私募股權領域有超過二十年的經驗，陳星豪共同創辦 Just Kitchen，他曾任多間加拿大投資代理商的資深執行專務和董事管理階層，擁有豐富的資本市場經驗。陳星豪擁有法學博士、經濟學和哲學學位。"
 }]
